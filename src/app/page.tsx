445873f1baac4aaad5d8f087208d9bfd241ebad2
Neelam More <neelammore1404@gmail.com>
2026-08-19 18:19:26 -0400

    Setup, Auth, Database

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+
+
+export default function Home(){
+  return(
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-background">
+      <h1 className="text-2xl font-semibold">
+        Welcome to Resonance
+      </h1>
+      <div>
+        <OrganizationSwitcher/>
+        <UserButton/>
+      </div>
+
+    </div>
+  )
+}
