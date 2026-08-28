a3ea290e93c91e29f49b92e3a2740c90e0be0b25
Neelam More <neelammore1404@gmail.com>
2026-08-28 00:36:57 -0400

    04: Backend Infrastructure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import { ClerkProvider} from "@clerk/nextjs"
+import { TRPCReactProvider } from "@/trpc/client";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -28,17 +29,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    
+    <ClerkProvider>
+    <TRPCReactProvider>
     <html lang="en">
       <body
         className={`${inter.variable} ${geistMono.variable} antialiased`}
       >
-        <ClerkProvider>
+        
         {children}
         <Toaster/>
-        </ClerkProvider>
+        
       </body>
     </html>
+    
+    </TRPCReactProvider>
+    </ClerkProvider>
     
   );
 }
